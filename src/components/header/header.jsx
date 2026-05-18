06c428af4252dfc5fe1812fa4157381db46bc948
aliciabuzeli <aliciabuzeli@gmail.com>
2026-05-18 11:25:56 -0300

    Add files via upload

---
+++ src/components/header/header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+    return (
+        <div>
+            <div>
+                <img src="./public.image 4.png" alt="logo" />
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </div>
+
+
+    )
+}
